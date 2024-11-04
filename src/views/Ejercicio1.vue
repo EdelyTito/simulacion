ea316366f929fa4c2e2eceeb53e5140aec375378
EdelyTito <edely.tito@ucb.edu.bo>
2024-11-04 10:10:45 -0400

    mejor asi nomas

--- src/views/Ejercicio1.vue
+++ src/views/Ejercicio1.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <h1>Simulación de Depósito a Plazo Fijo</h1>
+    <h1>Ejercicio A</h1>
     <div class="flex-container">
+      <div class="enunciado">
+        <Fieldset>
+          <p>
+            Simular el problema de depósito a plazo fijo para un capital de 1000, ¿en cuantos años se duplicaría el capital?
+          </p>
+        </Fieldset>
+      </div>
       <div class="controls">
         <label for="capitalInicial">Capital Inicial ($):</label>
-        <input type="number" v-model.number="capitalInicial" id="capitalInicial" min="1">
+        <input type="number" v-model="capitalInicial" id="capitalInicial" min="1">
         <label for="tiempoDeposito">Tiempo en años:</label>
-        <input type="number" v-model.number="tiempoDeposito" id="tiempoDeposito" min="1">
+        <input type="number" v-model="tiempoDeposito" id="tiempoDeposito" min="1">
         <label for="numSimulaciones">Número de Simulaciones:</label>
-        <input type="number" v-model.number="numSimulaciones" id="numSimulaciones" min="1">
+        <input type="number" v-model="numSimulaciones" id="numSimulaciones" min="1">
         <button @click="simulateInvestment">Simular Depósito</button>
         <button @click="resetResults">Limpiar</button>
       </div>
@@ -20,7 +27,7 @@
             <th>Simulación</th>
             <th>Capital ($)</th>
             <th>Interés ($)</th>
-            <th>Interés Total Acumulado ($)</th>
+            <th>Interés Total ($)</th>
           </tr>
         </thead>
         <tbody>
@@ -35,7 +42,7 @@
       <div class="averages">
         <p>Promedio Capital: ${{ avgCapital }}</p>
         <p>Promedio Interés: ${{ avgInteres }}</p>
-        <p>Promedio Interés Total Acumulado: ${{ avgIT }}</p>
+        <p>Promedio Interés Total: ${{ avgIT }}</p>
       </div>
     </Fieldset>
   </div>
@@ -71,7 +78,6 @@ export default {
         let totalInterest = 0;
         let interest = 0;
 
-        // Simulación del tiempo de depósito
         for (let year = 1; year <= this.tiempoDeposito; year++) {
           if (capital > 0 && capital <= 10000) {
             interest = capital * 0.035;
@@ -96,7 +102,6 @@ export default {
         });
       }
 
-      // Calcular los promedios
       this.avgCapital = (totalCapital / this.numSimulaciones).toFixed(2);
       this.avgInteres = (totalInterestSum / this.numSimulaciones).toFixed(2);
       this.avgIT = (totalITSum / this.numSimulaciones).toFixed(2);
